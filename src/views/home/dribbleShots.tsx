be4543a875712cb937c28ee7af36c33c02e427c9
karthickicp <karthickicp15@gmail.com>
2024-06-20 16:10:11 +0530

    Added dribble shots link

--- src/views/home/dribbleShots.tsx
+++ src/views/home/dribbleShots.tsx
@@ -1,8 +1,10 @@
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import dribbleShotOne from "@/assets/img/dribble/dribble-shot-1.png";
 import dribbleShotTwo from "@/assets/img/dribble/dribble-shot-2.png";
 import dribbleShotThree from "@/assets/img/dribble/dribble-shot-3.png";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import shareIcon from "@/assets/img/icons/share.svg";
 
 export const DribbleShots = () => {
   return (
@@ -10,7 +12,16 @@ export const DribbleShots = () => {
       <div className="container">
         <div className="titleSec">
           <p className="sec-category">WHAT’S THE LATEST?</p>
-          <h2 className="sec-title">Dribbble Shots</h2>
+          <div className="flex items-baseline">
+            <h2 className="sec-title">Dribbble Shots</h2>
+            <a href="https://dribbble.com/ashiksahan" target="_blank">
+              <img
+                src={shareIcon}
+                alt="share-icon"
+                className="ml-4 w-[26px] h-[26px] md:w-9 md:h-9"
+              />
+            </a>
+          </div>
         </div>
         <Swiper
           loop={true}
@@ -35,34 +46,34 @@ export const DribbleShots = () => {
           modules={[Pagination, Navigation, Autoplay]}
         >
           <SwiperSlide>
-          <div className="dribble-image-cover bg-[#E8ECED]">
-            <img
-              src={dribbleShotOne}
-              alt="dribble-shot-1"
-              loading="lazy"
-              className="dribble-images"
-            />
-          </div>
+            <div className="dribble-image-cover bg-[#E8ECED]">
+              <img
+                src={dribbleShotOne}
+                alt="dribble-shot-1"
+                loading="lazy"
+                className="dribble-images"
+              />
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="dribble-image-cover bg-[#F1F1F1]">
-            <img
-              src={dribbleShotTwo}
-              alt="dribble-shot-2"
-              loading="lazy"
-              className="dribble-images"
-            />
-          </div>
+            <div className="dribble-image-cover bg-[#F1F1F1]">
+              <img
+                src={dribbleShotTwo}
+                alt="dribble-shot-2"
+                loading="lazy"
+                className="dribble-images"
+              />
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-          <div className="dribble-image-cover bg-[#E2E4F0]">
-            <img
-              src={dribbleShotThree}
-              alt="dribble-shot-3"
-              loading="lazy"
-              className="dribble-images"
-            />
-          </div>
+            <div className="dribble-image-cover bg-[#E2E4F0]">
+              <img
+                src={dribbleShotThree}
+                alt="dribble-shot-3"
+                loading="lazy"
+                className="dribble-images"
+              />
+            </div>
           </SwiperSlide>
         </Swiper>
         {/* <div className="w-full float-left flex justify-between gap-[70px]">
