8a0320fe208340e7cc85a6a146e71fed6478c2a0
karthickicp <karthickicp15@gmail.com>
2024-06-30 14:38:26 +0530

    Aligned topsec like in figma

--- src/views/home/topSection.tsx
+++ src/views/home/topSection.tsx
@@ -4,7 +4,7 @@ import profilePicture from "@/assets/img/profile.png";
 
 export const TopSection = () => {
   return (
-    <div className="wrapper topSec relative mb-10 lg:mb-[130px]">
+    <div className="wrapper topSec relative">
       <h1
         className="w-full text-white -z-10 font-extrabold text-center absolute top-0 overflow-hidden text-[17vw] leading-[20vw] drop-shadow-[0_30px_100px_rgba(0,0,0,0.2)]
  md:drop-shadow-[0_30px_100px_rgba(0,0,0,0.1)] md:text-[17vw] md:leading-[17vw] 2xl:text-[300px] 2xl:leading-[280px] 2xl:drop-shadow-[0_30px_100px_rgba(0,0,0,0.08)]"
@@ -13,8 +13,8 @@ export const TopSection = () => {
       </h1>
       <div className="w-full float-left top-sec-wrapper ">
         <div className="container h-full">
-          <div className="flex flex-col-reverse items-center h-full descSec md:flex-row xl:items-end 2xl:relative">
-            <div className="flex-1">
+          <div className="flex flex-col-reverse h-full descSec md:flex-row 2xl:relative">
+            <div className="flex-1 self-center xl:self-end">
               <div className="flex items-center">
                 <img
                   src={hand}
@@ -28,13 +28,13 @@ export const TopSection = () => {
               <h1 className="text-[2.375rem] leading-[3.75rem] font-extrabold mb-6 text-black lg:text-[4.375rem] lg:leading-[5.625rem] lg:mb-10">
                 I'm Sr UI/UX Designer located in India
               </h1>
-              <p className="text-paragraph para-text leading-7 mb-6">
+              <p className="text-paragraph para-text leading-7 mb-6 lg:mb-[130px]">
                 I simplify digital experiences, making them user-friendly with
                 7+ years expertise. Successful in completing many projects, I
                 excel in both individual work and leading teams, sharing
                 insights.
               </p>
-              <div className="flex gap-4 md:hidden">
+              <div className="flex gap-4 mb-10 md:hidden ">
                 <OutlinedBtn
                   buttonText={"LinkedIn"}
                   link="https://www.linkedin.com/in/mohammad-ashik-m-3a9877198/"
@@ -45,9 +45,9 @@ export const TopSection = () => {
                 />
               </div>
             </div>
-            <div className="relative flex-1 flex justify-end topsec-img xl:translate-y-[200px] 2xl:absolute 2xl:right-0">
+            <div className="relative flex-1 flex justify-end topsec-img -z-10 self-center xl:self-baseline xl:translate-y-[50px] 2xl:absolute 2xl:right-0 2xl:bottom-0">
               <img src={profilePicture} alt="profile-picture" />
-              <div className="topsec-shadow w-full h-full absolute float-left"></div>
+              <div className="topsec-shadow w-full h-full absolute float-left translate-y-[70px] z-10"></div>
             </div>
           </div>
         </div>
